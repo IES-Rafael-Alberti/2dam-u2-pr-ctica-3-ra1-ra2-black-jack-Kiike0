89bc0b255e1e28c643d4067df5c29ac7e2b45dbc
Kiike0 <enriquevilrey@gmail.com>
2023-12-12 17:18:18 +0100

    Eliminado un import que no usaba

--- app/src/main/java/com/example/blackjack_jetpackcompose/screens/Menu.kt
+++ app/src/main/java/com/example/blackjack_jetpackcompose/screens/Menu.kt
@@ -1,7 +1,6 @@
 package com.example.blackjack_jetpackcompose.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
