ff174d55a6a5a7de5f1ebde0eaf96f3c703479c8
Kiike0 <enriquevilrey@gmail.com>
2023-12-13 03:05:54 +0100

    quitado imports.

--- app/src/main/java/com/example/blackjack_jetpackcompose/screens/Menu.kt
+++ app/src/main/java/com/example/blackjack_jetpackcompose/screens/Menu.kt
@@ -5,10 +5,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
